ead0bca88bb07ff9769574bb665237773065ca7b
Abram <58718191+4b3c@users.noreply.github.com>
2023-11-28 14:26:48 -0700

    fixed settings file issue (mostly by clearing cache)

--- shift/shift/settings.py
+++ shift/shift/settings.py
@@ -117,11 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# settings.py
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    # Path to your 'static' folder where CSS files are stored
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
