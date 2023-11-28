a71d203de55e5eac182c32fa519e7fd3bb1da461
Abram <58718191+4b3c@users.noreply.github.com>
2023-11-28 14:27:14 -0700

    copied login and register views from focal points

--- shift/hacks/views.py
+++ shift/hacks/views.py
@@ -1,5 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib import messages
 
-# Create your views here.
+
 def home(request):
     return render(request, 'hacks/home.html')
+
+
+def user_register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            # Redirect to a success page or home page
+            return redirect('file_list')  # Change 'home' to your desired URL name
+        else:
+            messages.error(request, 'Invalid username or password')
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'fonot/register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            # Redirect to a success page or home page
+            return redirect('file_list')  # Change 'home' to your desired URL name
+        else:
+            messages.error(request, 'Invalid username or password')
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'fonot/login.html', {'form': form})
+
+
+def user_logout(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect('home')
