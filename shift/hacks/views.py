70c811838ca0c8d9c2eb7cbfaf74412fab1ac85f
Abram <58718191+4b3c@users.noreply.github.com>
2023-12-07 14:55:58 -0700

    posting uses modals now, and removed unnecessary html files which are now modals

--- shift/hacks/views.py
+++ shift/hacks/views.py
@@ -11,7 +11,16 @@ def home(request):
     if request.user.is_authenticated:
         # should be changed eventually to show the specific user's feed
         shift_posts = Shift_post.objects.all()
-        return render(request, 'hacks/myhome.html', {'shift_posts': shift_posts})
+        if request.method == 'POST':
+            ns_form = ShiftPostForm(request.POST)
+            if ns_form.is_valid():
+                post = ns_form.save(commit=False)
+                post.author = request.user
+                post.save()
+                return redirect('home')
+        else:
+            ns_form = ShiftPostForm()
+        return render(request, 'hacks/myhome.html', {'shift_posts': shift_posts, 'ns_form': ns_form})
 
     l_form = AuthenticationForm()
     r_form = UserCreationForm()
@@ -40,36 +49,6 @@ def home(request):
     return render(request, 'hacks/home.html', {'shift_posts': shift_posts, 'l_form': l_form, 'r_form': r_form})
 
 
-def user_register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.error(request, 'Invalid username or password')
-    else:
-        form = UserCreationForm()
-    
-    return render(request, 'hacks/register.html', {'form': form})
-
-
-def user_login(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.error(request, 'Invalid username or password')
-    else:
-        form = AuthenticationForm()
-    
-    return render(request, 'hacks/login.html', {'form': form})
-
-
 def user_logout(request):
     if request.user.is_authenticated:
         logout(request)
@@ -78,17 +57,3 @@ def user_logout(request):
 
 def profile(request):
     return render(request, 'hacks/profile.html')
-
-
-def new_shift(request):
-    if request.method == 'POST':
-        form = ShiftPostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('home')
-    else:
-        form = ShiftPostForm()
-
-    return render(request, 'hacks/newshift.html', {'form': form})
